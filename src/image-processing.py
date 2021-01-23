f934ef8144be0b9ca053dc85cd8b0d431424ff86
edmondywen <edmondywen@gmail.com>
2021-01-23 15:37:06 -0800

    separated pdf into its pages. figuring out how to extract specific text from a page

--- src/image-processing.py
+++ src/image-processing.py
@@ -1,4 +1,13 @@
 import PyPDF2 as pypdf 
 
+#load in one pdf - los angeles
 losangeles = pypdf.PdfFileReader("mei-files/MEI-2020-Los-Angeles-California.pdf")
-print(losangeles.getNumPages()) 
+laNumPages = losangeles.getNumPages()
+
+#extract the text from each page
+textList = [""] #initialize empty textList
+for i in range(laNumPages - 1):
+    currPage = losangeles.getPage(i) #pages start indexed at 0
+    textList[i] = currPage.extractText
+
+#print(textList[0])
