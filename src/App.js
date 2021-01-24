ca736065f176fe8c66842f13cd971a9b491c36c6
Jiin <jiinkim02@gmail.com>
2021-01-23 16:24:26 -0800

    landing page except navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import logo from './images/logo.svg';
+import searchButton from './images/searchButton.svg';
+import CityCard from './CityCard.js';
+import LA from './images/LA.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          CitySpace
-        </h1>
       </header>
+      <body>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>CitySpace</h1>
+        <form className="search-bar">
+          <input type="text" className="search-bar-text"></input>
+          <input type="image" src={searchButton} alt="search"></input>
+        </form>
+        <div className="top-cities">
+          <h2>Top LGBTQ+ Friendly Cities:</h2>
+          <div className="city-card-list">
+            <CityCard city="Los Angeles, CA" total="108 / 100" stars={5} img={LA}/>
+            <CityCard city="Los Angeles, CA" total="108 / 100" stars={5} img={LA}/>
+            <CityCard city="Los Angeles, CA" total="108 / 100" stars={5} img={LA}/>
+            <CityCard city="Los Angeles, CA" total="108 / 100" stars={5} img={LA}/>
+            <CityCard city="Los Angeles, CA" total="108 / 100" stars={5} img={LA}/>
+          </div>
+        </div>
+      </body>
     </div>
   );
 }
---
+++ src/CityCard.js
@@ -0,0 +1,35 @@
+import './CityCard.css';
+import fiveStars from './images/stars/5stars.svg';
+import fourStars from './images/stars/4stars.svg';
+import threeStars from './images/stars/3stars.svg';
+import twoStars from './images/stars/2stars.svg';
+import oneStar from './images/stars/1star.svg';
+
+export default function CityCard(props) {
+
+    const stars = () => {
+        switch(props.stars) {
+            case 5: 
+                return (<img src={fiveStars} alt="five stars" className="city-card-rating"/>);
+            case 4:
+                return (<img src={fourStars} alt="four stars" className="city-card-rating"/>)
+            case 3:
+                return (<img src={threeStars} alt="three stars" className="city-card-rating"/>)
+            case 2:
+                return (<img src={twoStars} alt="two stars" className="city-card-rating"/>)
+            case 1:
+                return (<img src={oneStar} alt="one star" className="city-card-rating"/>)
+            default:
+                return ("error");
+        }
+    }
+
+    return (
+        <div className="city-card-container">
+            <h3 className="city-card-name">{props.city}</h3>
+            <img src={props.img} alt={props.city} className="city-card-image"/>
+            <div className="city-card-total">{props.total}</div>
+            {stars()}
+        </div>
+    );
+}
