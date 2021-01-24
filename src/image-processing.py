c3575334b0b6892e80707138669598e69c54d46e
edmondywen <edmondywen@gmail.com>
2021-01-23 17:24:48 -0800

    Implemented a way to extract the text out of each page in a document and store it into a python list

--- src/image-processing.py
+++ src/image-processing.py
@@ -3,11 +3,14 @@ import PyPDF2 as pypdf
 #load in one pdf - los angeles
 losangeles = pypdf.PdfFileReader("mei-files/MEI-2020-Los-Angeles-California.pdf")
 laNumPages = losangeles.getNumPages()
+laInfo = losangeles.getDocumentInfo()
 
-#extract the text from each page
-textList = [""] #initialize empty textList
-for i in range(laNumPages - 1):
+#separate each page into a list
+textList = ["", ""] #initialize empty textList. each document is 2 pages, so 2 items is enough.
+
+for i in range(laNumPages):
     currPage = losangeles.getPage(i) #pages start indexed at 0
-    textList[i] = currPage.extractText
+    textList[i] = currPage.extractText()
+    #print(textList[i])
+    i+=1
 
-#print(textList[0])
