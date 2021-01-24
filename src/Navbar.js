38d66796f0a045f9d9592a12fcd4228f82de1708
nareh <53708656+nareha@users.noreply.github.com>
2021-01-23 21:39:21 -0800

    Added link to home page on navbar icon

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import logo from './images/logo.svg';
 import './Navbar.css';
 import searchButton from './images/searchButton.svg';
@@ -6,7 +7,7 @@ import searchButton from './images/searchButton.svg';
 export default function NavBar() {
   return (
     <div className="navbar">
-        <img src={logo} alt="CitySpace Icon" className="logo" />
+        <Link to="/"><img src={logo} alt="CitySpace Icon" className="logo" /></Link>
         <h1 className="title">CitySpace</h1>
         <h2 className="subtitle">Live in your safe space.</h2>
         <form className="search-bar outline">
